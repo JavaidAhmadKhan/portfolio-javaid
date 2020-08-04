457a4b5fd8a97ef389cbb7a2b4a5fde402d6ebb8
JavaidAhmadKhan <relaxjavaid@gmail.com>
2020-08-04 20:51:06 +0530

    adding projects

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import initTilt from './js/tilt';
-import initSr from './js/sr';
+import initTilt from "./js/tilt";
+import initSr from "./js/sr";
 
-import './style/main.scss';
+import "./style/main.scss";
 
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
+$('a[href^="#"]').on("click", function (event) {
+  var target = $(this.getAttribute("href"));
   if (target.length) {
     event.preventDefault();
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
+    $("html, body").stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
   }
 });
 
